fc9aedf3f484847222738fc6d047f245ff240342
Jakob Hyldtoft <jh@konform.com>
2018-11-25 15:58:29 +0100

    add more app-icon definitions

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 const pkg = require('./package')
 
+let appName = 'Soundboard'
 let router = {}
 let baseUrl = '/';
 if (process.env.DEPLOY_ENV === 'GH_PAGES') {
@@ -17,28 +18,32 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: appName,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+
+      { hid: 'web-app-title', name: 'apple-mobile-web-app-title', content: appName },
+      { hid: 'application-name', name: 'application-name', content: appName },
+      { hid: 'ms-tilecolor', name: 'msapplication-TileColor', content: "#000000" },
+      { hid: 'msapp-config', name: 'msapplication-config', content: `${baseUrl}images/browserconfig.xml` },
+      { hid: 'theme-color', content: "#000000" },
     ],
     link: [
-      {
-        rel: 'icon', type: 'image/x-icon',
-        href: `${baseUrl}favicon.ico`
-      },
-      {
-        rel: 'manifest',
-        href: `${baseUrl}manifest.json`,
-      },
+      { rel: 'icon', type: 'image/x-icon', href: `${baseUrl}images/favicon.ico` },
+      { rel: 'manifest', href: `${baseUrl}site.webmanifest` },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: `${baseUrl}images/apple-touch-icon.png` },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: `${baseUrl}images/favicon-32x32.png` },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: `${baseUrl}images/favicon-16x16.png` },
+      { rel: 'mask-icon', type: 'image/png', href: `${baseUrl}images/safari-pinned-tab.svg`, color: '#000000' },
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#000' },
 
   /*
   ** Global CSS
